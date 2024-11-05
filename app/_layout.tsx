f5eb14d6ebecef35b414c4d8b82ad744980cc5da
phuhuy <phuhuyqhqb@gmail.com>
2024-11-05 09:08:05 +0700

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      <Stack.Screen name="get-code" options={{ headerShown: false }} />
+      <Stack.Screen name="verify-code" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
