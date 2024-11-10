9298028f1184e10e5a61cfafaedadd6e5d56c121
Cao Viet Anh <caovietanhbk@gmail.com>
2024-11-10 17:35:01 +0700

    add class statuses

--- components/ClassItem.tsx
+++ components/ClassItem.tsx
@@ -24,11 +24,24 @@ const ClassItem: React.FC<ClassItemProps> = ({
     });
   };
 
+  const statusText = (() => {
+    switch (status) {
+      case "ACTIVE":
+        return "Đang diễn ra";
+      case "UPCOMING":
+        return "Sắp diễn ra";
+      case "COMPLETED":
+        return "Đã kết thúc";
+      default:
+        return status;
+    }
+  })()
+
   return (
     <TouchableOpacity onPress={handlePress} style={styles.classItem}>
       <Image source={{ uri: avatarUrl }} style={styles.avatar} />
       <Text style={styles.className}>{name}</Text>
-      <Text style={styles.classStatus}>{status}</Text>
+      <Text style={styles.classStatus}>{statusText}</Text>
     </TouchableOpacity>
   );
 };
